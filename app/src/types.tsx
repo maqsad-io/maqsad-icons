7e8f1f66f02e5b6118c5c9b7bdeb8eb6c957fe01
Ahmad Feroz <akhmadferoz@gmail.com>
2026-01-21 12:58:50 +0500

    [FIX] Update default strokeWidth for system icons

--- app/src/types.tsx
+++ app/src/types.tsx
@@ -96,7 +96,7 @@ export function createSystemIcon(
         color,
         stroke,
         fill,
-        strokeWidth = 1,
+        strokeWidth = 2,
         className,
         style,
         ...props
